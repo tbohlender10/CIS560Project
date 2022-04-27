4be9cf2205fbbf4fe7b7d4014c5e7def439acc8b
tbohlender10 <tbohlender10@ksu.edu>
2022-04-26 20:50:13 -0500

    Added data for PlayerGameStatistics and GameSchool.

--- CIS560 Project/RebuildDatabase.ps1
+++ CIS560 Project/RebuildDatabase.ps1
@@ -78,7 +78,7 @@ Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData
 Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Data\Basketball.School.sql"
 Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Data\Basketball.Player.sql"
 Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Data\Basketball.Game.sql"
-Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Data\Basketball.PlayerSeasonStatistics.sql"
+Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Data\Basketball.PlayerGameStatistics.sql"
 Invoke-SqlCmd -ServerInstance $Server -Database $Database -InputFile "PersonData\Sql\Data\Basketball.GameSchool.sql"
 
 Write-Host "Rebuild completed."
